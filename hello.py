6c248fc96d6c8466baaed0f4473284d806be5986
Atul Mehta <atul.ze@gmail.com>
2023-04-27 22:40:45 +0530

    a

--- hello.py
+++ hello.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "<p>Hello .. Welcome to Udacity!</p>" 
+    return "<p>Hello .. Welcome to the World of Learning!</p>" 
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=80,debug=True) 
--- upload_docker.sh
+++ upload_docker.sh
@@ -5,12 +5,12 @@
 
 # Create dockerpath
 # dockerpath=<your docker ID/path>
-dockerpath="mehtaat/hello-udacity-app"
+dockerpath="mehtaat/hello-app"
   
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
 
 # Push image to a docker repository
 docker login --username=mehtaat
-docker tag hello-udacity-image:latest $dockerpath
+docker tag hello-image:latest $dockerpath
 docker push $dockerpath
